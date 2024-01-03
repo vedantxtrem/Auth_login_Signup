8aa996882339c659ef03c757d9ac9adecb3e00d5
Vedant Sahu <vedant@pop-os.localdomain>
2024-01-03 15:49:55 +0530

    pages

---
+++ client/auth/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Login from './component/Login'
+import Signup from './component/Signup';
+
+function App() {
+
+  return (
+    <>
+      <Login/>
+      <Signup/>
+    </>
+  )
+}
+
+export default App
